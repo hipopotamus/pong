0ee9f82841814a6511385839f2ad87c727b6e07a
hipopotamus <qkqntjddn@gmail.com>
2021-12-23 12:17:56 +0900

    first commit

---
+++ src/main/java/com/hipo/HipoApplication.java
@@ -0,0 +1,13 @@
+package com.hipo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HipoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HipoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hipo/HipoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hipo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HipoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
